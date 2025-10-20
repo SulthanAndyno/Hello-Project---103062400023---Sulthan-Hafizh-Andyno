822c63f337c71951f08c8235707d97b0d148d46b
SulthanHA <sulthandyno@gmail.com>
2025-10-20 22:49:34 +0700

    menambahkan detail pada about

--- src/components/About.js
+++ src/components/About.js
@@ -58,10 +58,7 @@ const About = () => {
               I’m an Information Technology student at Telkom University Jakarta, driven by curiosity and a love for creating interactive and meaningful digital experiences. My main focus is on front-end development, where I enjoy crafting interfaces that feel intuitive, responsive, and alive.
             </p>
             <p>
-              My journey began with a fascination for how design and logic can blend seamlessly to build something both beautiful and functional. Since then, I’ve been continuously learning, experimenting, and refining my skills from building dynamic layouts to improving user experiences through thoughtful design.
-            </p>
-            <p>
-              My goal is to grow into a developer who doesn’t just build products, but creates experiences — ones that connect people, simplify lives, and leave a lasting impression.
+              My goal is to grow into a developer who doesn’t just build products, but creates experiences ones that connect people, simplify lives, and leave a lasting impression.
             </p>
             <motion.a 
               href="https://drive.google.com/file/d/1tet08SQAtft1fdndzfc6hDv9Yg5T2NLs/view?usp=sharing"
